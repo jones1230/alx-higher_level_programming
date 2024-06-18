10542c4fd4c779d48af1b6196f97b72a78d0d743
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 14:17:09 -0700

    FIxed typoss

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const numOfArg = process.argv.length;
 const trueNumOfArg = numOfArg - 2;
-if (trueNumOfArg > 0) {
-  console.log('Arguments found');
-} else {
+if (trueNumOfArg === 0) {
   console.log('No argument');
-}
+} else if (trueNumOfArg > 0) {
+  console.log('Argument found');
+};
