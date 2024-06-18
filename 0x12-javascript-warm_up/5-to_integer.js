08040040446ceaa3c275932b8f9f6ad56b66160e
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 15:58:14 -0700

    Added space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const arg = process.argv[2];
 if (isNaN(parseFloat(arg))) {
   console.log('Not a number');
 } else {
- console.log('My number: ' + parseFloat(arg));
+  console.log('My number: ' + parseFloat(arg));
 }
