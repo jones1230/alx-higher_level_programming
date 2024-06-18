2cff6211b0cc9ee35f4cb1a3c37daac1173694d8
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 14:06:35 -0700

    Fixed typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const numOfArg = process.argv.lenghth;
-const trueNUmOfArg = numOfArg - 2;
-console.log(trueNUmOfArg);
+const numOfArg = process.argv.length;
+const trueNumOfArg = numOfArg - 2;
+console.log(trueNumOfArg);
