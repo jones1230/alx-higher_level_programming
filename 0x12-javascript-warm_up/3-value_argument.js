360b414cc912717d7bb2d8995224fc0a56a53a82
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 15:23:32 -0700

    removed brackets around typeof argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argPos = process.argv[2];
-if (typeof (argPos) === "undefined") {
+if (typeof argPos === "undefined") {
   console.log('No argument');
 } else {
   console.log(argPos);
