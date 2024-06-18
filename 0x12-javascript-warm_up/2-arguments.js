dfdd7dc390c4bb225d1adfc02236c17823bc9569
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 14:20:13 -0700

    Corrected flow

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const numOfArg = process.argv.length;
 const trueNumOfArg = numOfArg - 2;
 if (trueNumOfArg === 0) {
   console.log('No argument');
-} else if (trueNumOfArg > 0) {
+} else if (trueNumOfArg === 1) {
   console.log('Argument found');
-};
+} else {
+  console.log('Arguments found');
+}
