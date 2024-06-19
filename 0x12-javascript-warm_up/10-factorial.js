4f22d918113d6650f4891730f156a32523f1db8b
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-06-18 17:14:52 -0700

    Created task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const factorial = (n) => {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
+const result = factorial(num);
+console.log(`Factorial of ${num} is: ${result}`);
