2bdeaa6b87e23e50e487ffa79be05d1e9e5d97b0
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-08 17:12:31 -0700

    Changed code in Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,73 +1,18 @@
 #!/usr/bin/python3
 """ Finding peak """
-
 def find_peak(list_of_integers):
-    list_l = len(list_of_integers)
-    if list_l == 0:
+    if len(list_of_integers) == 0:
         return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
 
-
-def binary_search(a, lo, hi):
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return binary_search(a, mid + 1, hi)#!/usr/bin/python3
-
-
-def find_peak(list_of_integers):
-    list_l = len(list_of_integers)
-    if list_l == 0:
-        return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
-
-
-def binary_search(a, lo, hi):
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)#!/usr/bin/python3
-
-
-def find_peak(list_of_integers):
-    list_l = len(list_of_integers)
-    if list_l == 0:
-        return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
-
-
-def binary_search(a, lo, hi):
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)#!/usr/bin/python3
-
-
-def find_peak(list_of_integers):
-    list_l = len(list_of_integers)
-    if list_l == 0:
-        return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
-
-
-def binary_search(a, lo, hi):
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)
+        return find_peak(list_of_integers[mid + 1:])
