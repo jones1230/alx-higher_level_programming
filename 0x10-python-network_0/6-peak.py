105f3abbb76f0971d5953cf6afc4a1b4d5ac48fe
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-08 17:15:33 -0700

    Added documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Finding peak """
 def find_peak(list_of_integers):
+    """ Returning a peak in list of unsorted inegers """
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
