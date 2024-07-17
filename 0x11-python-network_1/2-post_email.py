2feee1c3edc835966228556e3c1d7d7b546b45b9
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-17 14:23:05 -0700

    Changed code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,6 +2,7 @@
 """
 script that takes in a URL and an email, sends a POST request to the passed
 URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
 """
 import urllib.request
 import urllib.parse
@@ -18,5 +19,8 @@ if __name__ == "__main__":
     data = query_string.encode("ascii")
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
+        # If you do not pass the data argument, urllib uses a GET request.
+        # One way in which GET and POST requests differ is that POST requests
+        # often have "side-effects".
         response_text = response.read().decode("utf-8")
         print(response_text)
