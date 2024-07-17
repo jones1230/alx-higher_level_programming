283b44e119178d0312c7fad7968885ac4b3c0b64
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-17 14:19:30 -0700

    Changed file name

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {
+        "Enter email": email
+    }
+    query_string = urllib.parse.urlencode(param)
+    data = query_string.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        response_text = response.read().decode("utf-8")
+        print(response_text)
