30f9543f6dd313c8d0d165997d2d54fab8ae1634
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-17 20:41:06 +0000

    Added task 0

---
+++ 0x011-pythion-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This script will fetch data from the
+    https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+with urllib.request.urlopen("https://alx-hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body)))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        
