658e2cc5048700a72af1846adabd4cd9de7a9683
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-31 21:35:43 +0000

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,30 +1,12 @@
 #!/usr/bin/node
+// Script that pinrts the title of Star Wars movie
+// where the episoe number mathces a given integer
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const filmId = process.argv[2];
 
-// Check if the movie ID is provided
-if (process.argv.length < 3) {
-  console.error('Usage: node <script-file> <ID>');
-  process.exit(1);
-}
-
-// Get the ID from command line arguments
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Make a GET request to the API endpoint
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error making request:', error);
-    process.exit(1);
-  }
-
-  // Parse the JSON string to object
-  try {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
-  } catch (error) {
-    console.error('Error parsing body:', error);
-    process.exit(1);
-  }
+request(url + filmId, (error, response, body) => {
+  if (error) throw error;
+  console.log(response && JSON.parse(body).title);
 });
